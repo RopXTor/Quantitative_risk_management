3e0aff97d71331f76d565110d97941a5cbb62fd0
RopXTor <julien.lorioux18@gmail.com>
2025-02-10 19:47:24 +0100

    -- update constants file

--- constants.py
+++ constants.py
@@ -1,3 +1,9 @@
+"""
+This file contains all constants used in this project
+"""
 from typing import Final
 
-CONFIG_FILE: Final[str] = "config.toml"
+TRADING_DAYS_PER_YEAR: Final[int] = 252
+CONFIG_FILE: Final[str] = r"config/config.toml"
+LOGGER_NAME: Final[str] = 'development_sorbonne'
+LOGGING_CONFIG_FILE: Final[str] = r'config/config_logging.yaml'
